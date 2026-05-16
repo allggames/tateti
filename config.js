c9eb37ff7902dd372ff6dd6650545194d3a1fe1d
TINI <agostinaanahi2001@gmail.com>
2026-05-16 17:04:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const SITE_CONFIG = {
     source: 'atenea',
-    chatUrl: 'https://www.casinoatenea.com/',
+    chatUrl: 'https://casinoatenea.club/',
     prizes: {
         1: '¡Hola! Gané el BONO DEL 100% + 1000 FICHAS en el Tatetí ⭕❌, quiero reclamarlo!',
         2: '¡Hola! Gané el BONO DEL 150% + 1500 FICHAS en el Tatetí ⭕❌, quiero reclamarlo!',
